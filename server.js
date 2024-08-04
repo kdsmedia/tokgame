bec5a202f4f6f7ebfc77c5b63eab4d2f3a66c8f4
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-08-05 02:42:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -18,33 +18,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 wss.on('connection', (ws) => {
   console.log('WebSocket connection established.');
 
+  // Buat koneksi ke TikTok Live
   const tiktokLiveConnection = new WebcastPushConnection(process.env.TIKTOK_USERNAME);
+  
   tiktokLiveConnection.connect().then(state => {
     console.info(`Listening to live data: ${state.roomId}`);
   }).catch(err => {
     console.error('Failed to connect:', err);
   });
 
+  // Kirim data chat ke klien WebSocket
   tiktokLiveConnection.on('chat', data => {
     data.type = 'chat';
     ws.send(JSON.stringify(data));
   });
 
+  // Kirim data like ke klien WebSocket
   tiktokLiveConnection.on('like', data => {
     data.type = 'like';
     ws.send(JSON.stringify(data));
   });
 
+  // Tangani penutupan koneksi WebSocket
   ws.on('close', () => {
     console.log('WebSocket connection closed');
     tiktokLiveConnection.disconnect(); // Disconnect when WebSocket connection is closed
   });
 
+  // Tangani error WebSocket
   ws.on('error', (error) => {
     console.error('WebSocket error:', error);
   });
 });
 
+// Jalankan server
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
